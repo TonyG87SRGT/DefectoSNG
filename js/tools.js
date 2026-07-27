bd7e5860bc8afddf6366f5ac650bc022aea3114c
TonyG87SRGT <tonygri87@gmail.com>
2026-07-27 22:23:12 +0500

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -77,28 +77,3 @@ function renderTools() {
 
 }
 
-function renderRingWeld() {
-
-  content.innerHTML = `
-
-    <div class="page-header">
-
-      <button class="back-button" id="back-button">
-        ‹
-      </button>
-
-      <div>
-        <h2>Длина кольцевого сварного шва</h2>
-        <p>Инструмент находится в разработке</p>
-      </div>
-
-    </div>
-
-  `;
-
-  document
-    .getElementById("back-button")
-    .addEventListener("click", renderTools);
-
-}
-  
